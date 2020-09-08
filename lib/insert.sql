bc7836345602cf0ce6e63c1441488bd13266c6b8
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-09-08 16:36:50 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ('Mr. Chocolate', 10,'male','brown','docile', 1, 1); 
+
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ( 'Rowdy', 3, 'male', 'black', 'playful', 1, 2);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Tabitha", 6, 'female', 'sun bear', 'docile', 1, 3);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ('Sergeant Brown', 20,'male','brown','lethargic', 0, 4);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Melissa", 13, 'female', 'brown', 'aggressive', 1, 5);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ('Grinch', 19, 'male', 'black', 'aggressive if provoked', 1, 6);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ("Wendy", 6, 'female', 'brown', 'docile', 1, 7);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES (NULL, NULL, NULL, NULL,'hostile; known casualties', NULL, 8);
