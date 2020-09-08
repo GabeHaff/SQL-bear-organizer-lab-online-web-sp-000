e0e36811ad6a21791626d6af94749c3a5b42d981
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-09-08 16:41:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT name, COUNT(color) AS prominentColor FROM bears GROUP BY color ORDER BY prominentColor DESC";
+  "SELECT color, COUNT(color) AS mostProminentColor FROM bears GROUP BY color ORDER BY mostProminentColor DESC";
 end
 
 def counts_number_of_bears_with_goofy_temperaments
